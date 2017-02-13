45c87b69696ea27c4d9a91a3a1da9dffdcef5941
Diego Mancevo <diegom@lnxdiegom.se.axis.com>
2017-02-13 15:39:56 +0100

    top_down_net ready for training

--- Networks/Top_Down_Teacher_Net/top_down_net.py
+++ Networks/Top_Down_Teacher_Net/top_down_net.py
@@ -3,11 +3,11 @@ import pickle as pkl
 import tensorflow as tf
 from sklearn.metrics import accuracy_score
 
-EPOCHS     = 25
+EPOCHS     = 5
 MINI_BATCH = 100
 FMP        = 1.414
-DEPTH      = 5
-CHANNELS   = 10
+DEPTH      = 7
+CHANNELS   = 30
 FC         = 128
 
 DATA_PATH               = "/notebooks/Data/top_down_view"
@@ -57,11 +57,7 @@ def conv(current, height, width, chan_in, chan_out, padding="SAME"):
 			strides=(1,1,1,1),
 			padding=padding,
 		)
-	# b       = tf.Variable(
-	# 	np.random.normal(0,0.01,size=chan_out),
-	# 	dtype=tf.float32
-	# 	)
-	# current = tf.nn.bias_add(current, b)
+
 	return current
 
 def graph(conv_depth, n, fc):
@@ -104,38 +100,88 @@ def graph(conv_depth, n, fc):
 	current = batch_norm(current, (1,1,fc))
 	current = conv(current, 1, 1, fc, 2)
 
-	return labels, images, keep_prob, augment, current
+	logits     = current[:,0,0,:]
+	prob       = tf.nn.softmax(logits, name="prob")
+	loss       = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+		logits, labels), name="loss")
+	train_step = tf.train.AdamOptimizer().minimize(loss, name="train_step")
 
-labels, images, keep_prob, augment, current = \
-graph(conv_depth=DEPTH, n=CHANNELS, fc=FC)
+	return labels, images, keep_prob, augment, logits, prob, loss, train_step
 
-init_op = tf.global_variables_initializer()
 
-print current.get_shape(), current.dtype
+if __name__ == '__main__':
 
-with open("{0}/train.pkl".format(DATA_PATH), "rb") as f:
-	train_labels, train_crops = pkl.load(f)
+	with open("{0}/train.pkl".format(DATA_PATH), "rb") as f:
+		train_labels, train_crops = pkl.load(f)
 
-with open("{0}/test.pkl".format(DATA_PATH), "rb") as f:
-	test_labels, test_crops = pkl.load(f)
+	with open("{0}/test.pkl".format(DATA_PATH), "rb") as f:
+		test_labels, test_crops = pkl.load(f)
 
-# N_train, N_test = len(train_labels), len(test_labels)
+	N_train, N_test = len(train_labels), len(test_labels)
 
-N_test = len(test_labels)
+	with tf.Session() as sess:
 
-with tf.Session() as sess:
-	
-	sess.run(init_op)
+		try:
+			saver = tf.train.import_meta_graph("saved/arch1/top_down_net.meta")
+			saver.restore(sess, tf.train.latest_checkpoint('saved/arch1/'))
+			layers = tf.get_collection('layers')
 
-	for mb in np.array_split(range(N_test), N_test/MINI_BATCH):
-	
-		C = sess.run(current, feed_dict={
-			labels: test_labels[mb],
-			images: test_crops[mb],
-			keep_prob: [0., .1, .2, .3, .4, .5, .5,],
-			augment:False,
-			})
+			print "Successfully loaded graph from file."
 
-		print C.shape
+		except IOError:
 
-		break
+			print "Building graph from scratch..."
+
+			layers = graph(conv_depth=DEPTH, n=CHANNELS, fc=FC)
+			for layer in layers:
+				tf.add_to_collection('layers', layer)
+
+			labels, images, keep_prob, augment, logits, prob, loss, train_step =\
+				layers
+
+			init_op    = tf.global_variables_initializer()
+			sess.run(init_op)
+
+
+		for epoch in range(EPOCHS):
+			print "Epoch: ", epoch+1
+			for __ in range(N_train/MINI_BATCH):
+
+				I = np.random.choice(range(N_train), size=100, replace=False)
+				sess.run(train_step, feed_dict={
+					labels: train_labels[I],
+					images: train_crops[I],
+					keep_prob: [0., .1, .2, .3, .4, .5, .5, .5, .5],
+					augment: True,
+				})
+			
+				train_score = sess.run(loss, feed_dict={
+					labels: train_labels[I],
+					images: train_crops[I],
+					keep_prob: [1. for i in range(DEPTH+2)],
+					augment: False,
+				})
+
+				J = np.random.choice(range(N_test), size=100, replace=False)
+				test_score = sess.run(loss, feed_dict={
+					labels: test_labels[J],
+					images: test_crops[J],
+					keep_prob: [1. for i in range(DEPTH+2)],
+					augment: False,
+				})
+
+				print "train: ", train_score, "test: ", test_score
+
+		J = np.random.choice(range(N_test), size=1000, replace=False)
+		y_hat = sess.run(prob, feed_dict={
+			labels: test_labels[J],
+			images: test_crops[J],
+			keep_prob: [1. for i in range(DEPTH+2)],
+			augment: False,
+		})
+
+		# new_saver = tf.train.Saver(max_to_keep=2)
+		# new_saver.save(sess, "saved/arch1/")
+
+print "Err: {0}".format(1-accuracy_score(test_labels[J],np.argmax(y_hat,axis=1)))
+print "Done!"
