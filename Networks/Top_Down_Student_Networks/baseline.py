8d6bffbe99b48f8c205d4e2ed358efbdb4be8c6b
Diego Mancevo <diegom@lnxdiegom.se.axis.com>
2017-02-14 14:26:28 +0100

    more networks

--- Networks/Top_Down_Student_Networks/baseline.py
+++ Networks/Top_Down_Student_Networks/baseline.py
@@ -3,15 +3,14 @@ import pickle as pkl
 import tensorflow as tf
 from sklearn.metrics import accuracy_score, f1_score
 
-EPOCHS     = 5
-MINI_BATCH = 100
-FMP        = 1.414
-DEPTH      = 8
-CHANNELS   = 10
-
 DATA_PATH               = "/notebooks/Data/top_down_view"
 CROP_HEIGHT, CROP_WIDTH = 60, 80
 
+EPOCHS     = 10
+DEPTH      = 1
+CHANNELS   = 30
+MINI_BATCH = 100
+MP         = (5,)
 
 def batch_norm(layer, dims):
 	'''
@@ -68,54 +67,54 @@ def leaky_relu(current):
 
 def graph():
 	'''
-	Build Graph.
+	Shallow and wide baseline network.
 	'''
 
-	# Load the data...
 	labels       = tf.placeholder(dtype=tf.int32, shape=(None,), name="labels")
 	images       = tf.placeholder(dtype=tf.float32,
 		shape=(None,CROP_HEIGHT,CROP_WIDTH,3), name="images")
-	keep_prob    = tf.placeholder(dtype=tf.float32, shape=(DEPTH+2,))
+	keep_prob    = tf.placeholder(dtype=tf.float32, shape=(DEPTH,))
 
 
 	# Data Augmentation
 	augment = tf.placeholder(tf.bool, name="augment")
 	current = tf.cond(augment, lambda: data_aug(images), lambda: images)
-	
-	height, width = CROP_HEIGHT,CROP_WIDTH
+
+	height, width     = CROP_HEIGHT,CROP_WIDTH
 	chan_in, chan_out = 3, CHANNELS
+
 	for i in range(1,DEPTH+1):
 		chan_out = i*CHANNELS
-		current  = batch_norm(current, (height,width,chan_in))
-		current  = conv(current, 2, 2, chan_in, chan_out)
-		current  = leaky_relu(current)
-		curent   = tf.nn.dropout(current, keep_prob[i-1])
-		current  = tf.nn.fractional_max_pool(
-			value=current,
-			pooling_ratio=(1.0, FMP, FMP, 1.0),
-			pseudo_random=True,
-			)[0]
+		current = batch_norm(current, (height, width, chan_in))
+		current = conv(current, 2, 2, chan_in, chan_out)
+		current = leaky_relu(current)
+		curent  = tf.nn.dropout(current, keep_prob[i-1])
+		current = tf.nn.max_pool(
+					value  =current,
+					ksize  =(1,MP[i-1],MP[i-1],1),
+					strides=(1,MP[i-1],MP[i-1],1),
+					padding="SAME"
+				)
+
 		chan_in       = i*CHANNELS
-		height, width = int(height/FMP), int(width/FMP)
+		height, width = height/MP[i-1], width/MP[i-1]
 
-	current = batch_norm(current, (height,width,chan_in))
-	current = conv(current, height, width, chan_in, chan_in, padding="VALID")
+	current = batch_norm(current, (height, width, chan_in))
+	current = conv(current, height, width, chan_in, 2, padding="VALID")
 	current = leaky_relu(current)
-	current = tf.nn.dropout(current, keep_prob[i])
 
-	current = batch_norm(current, (1,1,chan_in))
-	current = conv(current, 1, 1, chan_in, 2)
-
-	logits     = current[:,0,0,:]
-	prob       = tf.nn.softmax(logits, name="prob")
-	loss       = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+	logits  = current[:,0,0,:]
+	prob    = tf.nn.softmax(logits, name="prob")
+	loss    = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
 		logits, labels), name="loss")
 	train_step = tf.train.AdamOptimizer().minimize(loss, name="train_step")
 
-	return labels, images, keep_prob, augment, logits, prob, loss, train_step
-
+	return labels, images, keep_prob, augment, prob, loss, train_step
 
 if __name__ == '__main__':
+
+
+	labels, images, keep_prob, augment, prob, loss, train_step = graph()
 
 	with open("{0}/train.pkl".format(DATA_PATH), "rb") as f:
 		train_labels, train_crops = pkl.load(f)
@@ -124,29 +123,11 @@ if __name__ == '__main__':
 		test_labels, test_crops = pkl.load(f)
 
 	N_train, N_test = len(train_labels), len(test_labels)
-
+	
 	with tf.Session() as sess:
 
-		try:
-			saver = tf.train.import_meta_graph("saved/arch1/top_down_net.meta")
-			saver.restore(sess, tf.train.latest_checkpoint('saved/arch1/'))
-			layers = tf.get_collection('layers')
-
-			print "Successfully loaded graph from file."
-
-		except IOError:
-
-			print "Building graph from scratch..."
-
-			layers = graph()
-			for layer in layers:
-				tf.add_to_collection('layers', layer)
-
-			labels, images, keep_prob, augment, logits, prob, loss, train_step =\
-				layers
-
-			init_op    = tf.global_variables_initializer()
-			sess.run(init_op)
+		init_op = tf.global_variables_initializer()
+		sess.run(init_op)
 
 		for epoch in range(EPOCHS):
 			for __ in range(N_train/MINI_BATCH):
@@ -155,26 +136,9 @@ if __name__ == '__main__':
 				sess.run(train_step, feed_dict={
 					labels: train_labels[I],
 					images: train_crops[I],
-					keep_prob: [0., .1, .2, .3, .4, .5, .5, .5, .5, .5],
+					keep_prob: [0.5],
 					augment: True,
 				})
-			
-				# train_score = sess.run(loss, feed_dict={
-				# 	labels: train_labels[I],
-				# 	images: train_crops[I],
-				# 	keep_prob: [1. for i in range(DEPTH+2)],
-				# 	augment: False,
-				# })
-
-				# J = np.random.choice(range(N_test), size=100, replace=False)
-				# test_score = sess.run(loss, feed_dict={
-				# 	labels: test_labels[J],
-				# 	images: test_crops[J],
-				# 	keep_prob: [1. for i in range(DEPTH+2)],
-				# 	augment: False,
-				# })
-
-				# print "train: ", train_score, "test: ", test_score
 
 			y_hat = np.empty(shape=(0,2))
 			for J in np.array_split(range(N_test),  15):
@@ -182,14 +146,10 @@ if __name__ == '__main__':
 					y_hat, sess.run(prob, feed_dict={
 					labels: test_labels[J],
 					images: test_crops[J],
-					keep_prob: [1. for i in range(DEPTH+2)],
+					keep_prob: [1.],
 					augment: False,
 				})))
 
 			print "Epoch: ", epoch+1
 			print "Err: ", 1-accuracy_score(test_labels,np.argmax(y_hat,axis=1))
 			print "F1 Score: ", f1_score(test_labels,np.argmax(y_hat,axis=1))
-		# new_saver = tf.train.Saver(max_to_keep=2)
-		# new_saver.save(sess, "saved/arch1/")
-
-print "Done!"
